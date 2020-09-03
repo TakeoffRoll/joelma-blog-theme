df6abf072aeb3e57f0bdecc4adc83bb5b487f77f
Noah Williams <public@noahsbwilliams.com>
2020-09-03 11:54:25 -0800

    Remove protocol from config file

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,6 +4,6 @@ var config = {
     'infinite-scroll': false,
     'infinite-scroll-step': 1,
     'disqus-shortname': 'takeoffroll',
-    'content-api-host': 'https://takeoffroll.com',
+    'content-api-host': 'takeoffroll.com',
     'content-api-key': '6b0bde4683001d8ea5721473fc',
 };
