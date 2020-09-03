e3852558db8cd9b975944d64350792dd2cadae98
Noah Williams <public@noahsbwilliams.com>
2020-09-03 10:48:06 -0800

    Add disqus shortname

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ var config = {
     'load-more': false,
     'infinite-scroll': false,
     'infinite-scroll-step': 1,
-    //    'disqus-shortname': 'hauntedthemes-demo',
+    'disqus-shortname': 'takeoffroll',
     'content-api-host': 'https://takeoffroll.com',
     'content-api-key': '6b0bde4683001d8ea5721473fc',
 };
